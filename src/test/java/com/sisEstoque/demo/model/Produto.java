db505ccaa0cbd063c717a4e27563ede2790ea83d
Danilo Almeida dos Santos <danilo1978tw@gmail.com>
2026-09-17 16:08:52 -0300

    feat: cria entidade Produto com mapeamentos JPA

---
+++ src/test/java/com/sisEstoque/demo/model/Produto.java
@@ -0,0 +1,60 @@
+package com.sisEstoque.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private int id;
+    private String nome;
+    private double preco;
+    private int quantidade;
+
+    public Produto() {
+    }
+    public Produto(int id, String nome, double preco, int quantidade) {
+        this.id = id;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+}
